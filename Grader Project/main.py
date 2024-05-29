90c418a0387d1c112775fe6fbf14420c8ebf9a2d
Donald Ipperciel <dipper@yorku.ca>
2024-05-29 17:34:14 -0400

    Added the following functions: 1. Checking if citations match reference in bibliography 2. Checking if a reference in the bibliography has at least one citation in the text. 3. Generate a final report

--- Grader Project/main.py
+++ Grader Project/main.py
@@ -9,8 +9,9 @@ import re
 
 # INPUT variables------------------------------------------------------------------------------------ #
 
-doc = Document("C:/Users/donal/OneDrive - York University/New/Al/E-Grader/test_apa.docx") #Document to be graded
+doc = Document("C:/Users/donal/OneDrive - York University/New/Al/E-Grader/test_concordance.docx") #Document to be graded
 end_of_paragraph = r"\.\s*$" #Regex pattern to find the end of a paragraph (a hard return)
+required_wordcount = 1000
 
 # Functions------------------------------------------------------------------------------------------ #
 
@@ -19,7 +20,7 @@ def create_three_sections(doc, end_of_paragraph): #  Splits the text into three 
     text = ""  # Variable to hold the full text of the document
     found_beginning = False # Variable to check if the beginning of the post-title text portion of the document has been found
     for paragraph in doc.paragraphs:
-        if re.search(end_of_paragraph, paragraph.text) and found_beginning == False: # If there is a real end of a paragraph
+        if re.search(end_of_paragraph, paragraph.text) and found_beginning == False: # If there is a period and hard return, that's the first paragraph of the body
             text += "*Beginning*" + paragraph.text + "\n"
             found_beginning = True  # Set the flag to true to indicate that the beginning of the text portion of the document has been found and marked
         else:
@@ -53,82 +54,187 @@ def wordcount(text): #  Returns the number of words in a string
     word_count = len(word_list)  # Count the number of words in the list
     return word_count
 
+def check_intext_citations(body):
+    # pattern = r"\(([^)]*), (\d{4})(?:, p\. (\d{1,4}))?\)" # e.g. (Ipperciel, 2018, p. 12). THIS IS TOO PRECISE. WON'T PICK UP ON MISTAKES
+    pattern = r"\([^)]*\d{4}[a-zA-Z]?[^)]*\)"
+    citation_list = re.findall(pattern, body) # finds all in-text citations in the body
+    # print("citation_list: ", citation_list)
+
+
+    error_APA = []
+    i = 0
+    # all rules for in-text citations in if clauses
+    for citation in citation_list:
+        if not re.search(", \d{4}", citation):  #  check if there's a comma and space before the year
+            error_APA.append("Missing comma before year in reference " + str(i + 1) + ": " + citation_list[i])
+        if re.search("\d{4}, ?\d{1,4}", citation):  # check if there's a space and comma after the year, and only a number instead of p.
+            error_APA.append("Missing 'p. ' before page number in reference " + str(i + 1) + ": " + citation_list[i])
+        if not re.search("\d{4}[a-zA-Z]?[,)]", citation): # check if there's a missing comma after the year
+            error_APA.append("Missing comma after the year in reference " + str(i + 1) + ": " + citation_list[i])
+        if "et al" in citation_list[i]:  # Check "et al" mistakes
+            if "et al." not in citation_list[i]:
+                error_APA.append("Missing period after 'et al' in reference " + str(i + 1) + ": " + citation_list[i])
+            if ", et al" in citation_list[i] or ",et al." in citation_list[0]:
+                error_APA.append(
+                    "No comma should precede 'et al.' (the comma always follows 'et al.'). See reference " + str(
+                        i + 1) + ": " + citation_list[i])
+        if " and " in citation_list[i]:  # Check if "and" instead of & is in the citation
+            error_APA.append(
+                "Ampersand (&) should be used instead of 'and' in reference " + str(i + 1) + ": " + citation_list[i])
+        # Next: check if authors are juxtaposed with comma instead of & !!!!
+
+        i = i + 1
+
+    # Do citations match with the references? !!!!!!!
+
+    return error_APA, citation_list
+
+def check_references(references):
+    error_references = []
+
+    # Find number of references
+    reference_list = references.splitlines()  # Get the individual references in a list
+    reference_list = [item for item in reference_list if item]
+    num_references = len(reference_list)
+
+    index1 = reference_list[0].index(").")  # testing
+
+    ref_author_year_part = reference_list[0][:index1 + 2]
+    # print(ref_author_year_part)
+
+    # find if thers's leading space in ref_author_year_part. If not: Message error: Missing space between year and title. !!!
+
+    # Author check
+    references = references.lstrip()  # Remove  whitespace from the references section
+    test = "Ipperciel, D., Elatia, S. (2018a)."
+
+    #### Define regex patterns for APA ####
+    APA_author = r"^\b([A-Z][a-z]*),\s([A-Z])\."  # e.g. Ipperciel, D.
+    APA_author_next = r"\,\s([A-Z][a-z]*),\s([A-Z])\.\s"  # e.g. & ElAtia, S.
+    APA_author_all = f"{APA_author}(?:{APA_author_next})*"  # e.g. Ipperciel, D. & ElAtia, S.
+    APA_year = r"\([0-9]{4}[a-z]?\)\."
+
+    ### Checking accuracy of author format
+    # if re.search(APA_author, test): #If the test string matches the pattern
+    #    print("Author format is correct")
+    if re.search(APA_author_all, test):
+        pass
+        #print("Author format is also correct")
+    else:  # If the test string does not match the pattern
+        pass
+        # print("Author format is incorrect")
+
+    # Check for missing period ad the end of the reference
+    # Check for alphabetical order in references
+
+    return error_references, num_references
+
+
+def concordance_btw_citations_and_references(references, citation_list): # Checks if the citations in the body match the references and visa versa
+    error_concordance = []
+
+    # Get the individual references in a list
+    reference_list = references.splitlines()
+    reference_list = [item for item in reference_list if item]
+    #num_references = len(reference_list)
+
+    # Grab the names and authors from each reference
+    ref_author_year_part = []
+    i = 0
+    for i in range(len(reference_list)):
+        index1 = reference_list[i].index(").")
+        ref_author_year_part.append(reference_list[i][:index1 + 1])
+        i = i + 1
+
+    # The ref_author_year_part list must be cleaned up to look like a proper in-text citation
+    cleaned_ref_author_year = []
+    for i in range(len(ref_author_year_part)):
+        temp_item = ref_author_year_part[i].split()
+        if len(temp_item) == 3:  # if the reference only has one author, e.g. Ipperciel, D. (2010) -- 3 parts
+            cleaned_ref_author_year.append(temp_item[0] + " " + temp_item[2][1:-1])
+        elif len(temp_item) == 6:  # if the reference has two authors, e.g. Ipperciel, D. & Elatia, S. (2010) -- 6 parts
+            cleaned_ref_author_year.append(temp_item[0][:-1] + " " + temp_item[2] + " " + temp_item[3] + " " + temp_item[5][1:-1])
+        elif len(temp_item) > 6: # if the reference has more than two authors, e.g. Ipperciel, D., Elatia, S. & Johnson, M. (2010)
+            cleaned_ref_author_year.append(temp_item[0][:-1] + " et al., " + temp_item[-1][1:-1])
+        i = i + 1
+
+    # Compare the citation_list with cleaned references
+    for i in range(len(citation_list)):
+        for j in range(len(cleaned_ref_author_year)):
+            citation_year = re.search("\d{4}", citation_list[i]).group()
+            if (citation_list[i].split(",")[0].strip()[1:] not in cleaned_ref_author_year[j] # first condition: first word in citation_list is in reference
+                    or citation_year not in cleaned_ref_author_year[j]):  # second condition: the year in citation is in reference
+                in_the_references = 0
+            else:
+                in_the_references = 1
+                break
+        if in_the_references == 0:
+            error_concordance.append("Citation " + str(i + 1) + " " + citation_list[i] + " is not in the references.")
+
+    # Compare the cleaned references with the citation_list (there must be at least one citation for each reference)
+    for i in range(len(cleaned_ref_author_year)):
+        in_the_citations = 0
+        for j in range(len(citation_list)):
+            ref_year = re.search("\d{4}", cleaned_ref_author_year[i]).group()
+            if cleaned_ref_author_year[i].split(",")[0].strip() in citation_list[j] and ref_year in citation_list[j]:
+                in_the_citations = 1
+                break
+        if in_the_citations == 0:
+            error_concordance.append("Reference " + str(i + 1) + " " + cleaned_ref_author_year[i] + " is not used as a citation in the body.")
+
+    return error_concordance
+
+
+def generate_final_report(required_wordcount, num_words_text_minus_title, error_APA, error_references, error_concordance):
+   final_report = ""
+   if num_words_text_minus_title < (required_wordcount*0.9):
+       final_report = final_report + "Your text has fewer words than the required word count (i.e. " + str(required_wordcount) + " words minus 10% or " + str(int(required_wordcount*0.9)) + " words).\n"
+   if num_words_text_minus_title > (required_wordcount * 1.1):
+       final_report = final_report + "Your text has more words than the required word count (i.e. " + str(required_wordcount) + " words plus 10% or " + str(int(required_wordcount * 1.1)) + " words).\n"
+   if error_APA:
+       final_report = final_report + "APA citation error(s): \n"
+       for i in range(len(error_APA)):
+           final_report = final_report + error_APA[i] + "\n"
+
+   if error_references:
+       final_report = final_report + "Bibliography error(s): \n"
+       for i in range(len(error_references)):
+           final_report = final_report + error_references[i] + "\n"
+
+   if error_concordance:
+       for i in range(len(error_concordance)):
+           final_report = final_report + error_concordance[i] + "\n"
+
+   return final_report
 
 # Main ---------------------------------------------------------------------------------------------- #
 # --------------------------------------------------------------------------------------------------- #
 
-title_page, text_minus_title, references, body, full_text = create_three_sections(doc, end_of_paragraph) # Creation of title page, text minus title page and reference from the document
-
-# First have to test if there's a bibliography. If not, it should be added to the report !!!!! And bypass References check
+# Creation of title page, text minus title page and reference from the document
+title_page, text_minus_title, references, body, full_text = create_three_sections(doc, end_of_paragraph)
 
 # Word counts --------------------------------------------------------------------------------------- #
 num_words_text_minus_title = wordcount(text_minus_title)
 num_body = wordcount(body)
 num_words_full_text = wordcount(full_text) - 1 #Count the number of words in the full text of the document. -1 is to remove "*Beginning*" [SHOULD I DELETE THE WORD?*****]
 
-# Check APA style ----------------------------------------------------------------------------------- #
+# Error report for in-text citations ----------------------------------------------------------------- #
+error_APA, citation_list = check_intext_citations(body)
+#print(error_APA)
+
+# Are the in-text citations in references and vice versa? -------------------------------------------- #
+error_concordance = concordance_btw_citations_and_references(references, citation_list)
+
+# Error report for references ------------------------------------------------------------------------ #
 error_references = []
-if references != "null":
-    reference_list = references.splitlines()  # Get the individual references in a list
-    reference_list = [item for item in reference_list if item]
-    num_references = len(reference_list)
-
-    index1 = reference_list[0].index(").") # testing
-
-    ref_author_year_part = reference_list[0][:index1+2]
-    print(ref_author_year_part)
+if references != "null": # Do this only if there is a references section
+    error_references, num_references = check_references(references)
 else:
     error_references.append("There is no references section")
+# print("Error report for references: ", error_references)
+# print("Number of references: ", num_references)
 
-# find if thers's leading space in ref_author_year_part. If not: Message error: Missing space between year and title. !!!
+# Generate final report ------------------------------------------------------------------------------ #
 
-# In-text citations
-# pattern = r"\(([^)]*), (\d{4})(?:, p\. (\d{1,4}))?\)" # e.g. (Ipperciel, 2018, p. 12). THIS IS TOO PRECISE. WON'T PICK UP ON MISTAKES
-pattern = r"\([^)]*\d{4,}[^)]*\)"
-citation_list = re.findall(pattern, body)
-print("citation_list: ", citation_list)
-
-# First check if there are three parts in the matches!!! If not: missing comma after author
-
-error_APA = []
-i = 0
-for citation in citation_list:
-    if not re.search(", \d{4}", citation):  # first check if there's a comma and space before the year
-        error_APA.append("Missing comma before year in reference " + str(i+1) + ": " + citation_list[i])
-    if re.search("\d{4}, \d{1,2,3,4}", citation):  # check if there's a space and comma after the year, and only a number instead of p.
-        error_APA.append("Missing 'p. ' before page number in reference " + str(i+1) + ": " + citation_list[i])
-    if "et al" in citation_list[i]:  # Check "et al" mistakes
-        if "et al." not in citation_list[i]:
-            error_APA.append("Missing period after 'et al' in reference " + str(i+1) + ": " + citation_list[i])
-        if ", et al" in citation_list[i] or ",et al." in citation_list[0]:
-            error_APA.append("No comma should precede 'et al.' (the comma always follows 'et al.'). See reference " + str(i+1) + ": " + citation_list[i])
-    if " and " in citation_list[i]:  # Check if "and" instead of & is in the citation
-        error_APA.append("Ampersand (&) should be used instead of 'and' in reference " + str(i + 1) + ": " + citation_list[i])
-    # Next: check if authors are juxtaposed with comma instead of & !!!!
-
-    i = i + 1
-print(error_APA)
-
-# Do citations match with the references? !!!!!!!
-
-# Author check
-references = references.lstrip() #Remove  whitespace from the references section
-test = "Ipperciel, D., Elatia, S. (2018a)."
-
-
-
-
-
-#### Define regex patterns for APA ####
-APA_author = r"^\b([A-Z][a-z]*),\s([A-Z])\." # e.g. Ipperciel, D.
-APA_author_next = r"\,\s([A-Z][a-z]*),\s([A-Z])\.\s" # e.g. & ElAtia, S.
-APA_author_all = f"{APA_author}(?:{APA_author_next})*" # e.g. Ipperciel, D. & ElAtia, S.
-APA_year = r"\([0-9]{4}[a-z]?\)\."
-
-### Checking accuracy of author format
-#if re.search(APA_author, test): #If the test string matches the pattern
-#    print("Author format is correct")
-if re.search(APA_author_all,test):
-    print("Author format is also correct")
-else: #If the test string does not match the pattern
-    print("Author format is incorrect")
+final_report = generate_final_report(required_wordcount, num_words_text_minus_title, error_APA, error_references, error_concordance)
+print("Final report: ", final_report)
